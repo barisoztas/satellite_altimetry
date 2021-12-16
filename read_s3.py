2cd86b79e786d4b350a75d1369d3ace1aecaf605
baris.oztas@metu.edu.tr <Aklandsis1234?!>
2021-12-16 09:35:41 +0300

    range corrections

--- read_s3.py
+++ read_s3.py
@@ -12,11 +12,11 @@ class s3_extraction(object):
     def __init__(self):
         self.start_time = datetime.datetime.now()
         self.end_time = None
-        self.input_folder = r"C:\Users\baris\OneDrive - metu.edu.tr\CE-STAR\Sentinel3\data\non time critical\beysehir"
+        self.input_folder = r"/home/hsaf/Baris/Sentinel3/s3_l2/nontimecritical/burdur"
         self.nc_list = []
         self.xlsx_list_20hz = []
         self.xlsx_list_1hz = []
-        self.output_folder = r"C:\Users\baris\OneDrive - metu.edu.tr\CE-STAR\Sentinel3\data\non time critical\beysehir\output"
+        self.output_folder = r"/home/hsaf/Baris/Sentinel3/s3_l2/nontimecritical/burdur/output"
 
     def calculate_time(self):
         self.end_time = datetime.datetime.now()
@@ -24,15 +24,15 @@ class s3_extraction(object):
 
     def find_nc_files(self):
         self.nc_list=[]
-        for nc_file in pathlib.Path(self.input_folder).glob('**\\standard_measurement.nc'):
+        for nc_file in pathlib.Path(self.input_folder).glob('**/standard_measurement.nc'):
             self.nc_list.append(nc_file)
         return self.nc_list
 
     def find_xlsx_files(self):
-        for xlsx_20 in pathlib.Path(self.input_folder).glob(('**\\*20hz.xlsx')):
+        for xlsx_20 in pathlib.Path(self.input_folder).glob(('**/*20hz.xlsx')):
             self.xlsx_list_20hz.append(xlsx_20)
 
-        for xlsx_1 in pathlib.Path(self.input_folder).glob(('**\\*1hz.xlsx')):
+        for xlsx_1 in pathlib.Path(self.input_folder).glob(('**/*1hz.xlsx')):
             self.xlsx_list_1hz.append(xlsx_1)
         return self.xlsx_list_20hz, self.xlsx_list_1hz
 
@@ -42,8 +42,8 @@ class s3_extraction(object):
             counter = counter+1
             roi_points_20, roi_points = s3_extraction.read_nc(nc)
             write_path = os.path.split(nc)[0]
-            writer_20hz = pd.ExcelWriter(write_path+"\\output_20hz.xlsx", engine='xlsxwriter')
-            writer_1hz = pd.ExcelWriter(write_path+"\\output_1hz.xlsx", engine='xlsxwriter')
+            writer_20hz = pd.ExcelWriter(write_path+"/output_20hz.xlsx", engine='xlsxwriter')
+            writer_1hz = pd.ExcelWriter(write_path+"/output_1hz.xlsx", engine='xlsxwriter')
             roi_points.to_excel(writer_1hz)
             writer_1hz.save()
             roi_points_20.to_excel(writer_20hz)
@@ -59,7 +59,9 @@ class s3_extraction(object):
         alt = list(d.variables["alt_01"])
         wet = list(d.variables["rad_wet_tropo_cor_01_ku"])  # values are minus, so add to range
         wet_model = list(d.variables["mod_wet_tropo_cor_zero_altitude_01"])  # values are minus, so add to range
+        wet_model_2 = list(d.variables["mod_wet_tropo_cor_meas_altitude_01"])  # values are minus, so add to range
         dry = list(d.variables["mod_dry_tropo_cor_zero_altitude_01"])  # values are minus, so add to range
+        dry_2 = list(d.variables["mod_dry_tropo_cor_meas_altitude_01"])  # values are minus, so add to range
         tide1 = list(d.variables["solid_earth_tide_01"])  # extract from range
         tide2 = list(d.variables["pole_tide_01"])  # extract from range
         geoid = list(d.variables["geoid_01"])
@@ -70,7 +72,7 @@ class s3_extraction(object):
         lon20 = list(d.variables["lon_20_ku"])
         alt20 = list(d.variables["alt_20_ku"])
         range20 = list(d.variables["range_ice_sheet_20_ku"])
-        # iono20 = list(d.variables["iono_cor_alt_20_ku"])                      # values are minus, so add to range
+        iono20 = list(d.variables["iono_cor_alt_20_ku"])                      # values are minus, so add to range
         # #check whether it is usable for lakes or not
 
         #########################################################
@@ -80,27 +82,27 @@ class s3_extraction(object):
                                   'lon20': lon20,
                                   'time20': time20,
                                   'alt20': alt20,
-                                  'range20': range20})  # ,
-        # 'iono20': iono20})
+                                  'iono20':iono20,
+                                  'range20': range20})
         points = pd.DataFrame({'lat': lat,
                                'lon': lon,
                                'time': time,
                                'alt': alt,
                                'wet': wet,
                                'wet_model': wet_model,
+                               'wet_model_measurement_altitude': wet_model_2,
                                'tide1': tide1,
                                'tide2': tide2,
                                'dry': dry,
+                               'dry_measurement_altitude': dry_2,
                                'geoid': geoid,
                                'iono': iono})  # values are minus, so add to range
-        #roi_points_20 = points_20[((points_20['lat20'] < 37.74188) & (points_20['lat20'] > 37.66296) & (
-        #        points_20['lon20'] < 30.13465) & (points_20['lon20'] > 30.08734))]  # burdur
-        #roi_points = points[((points['lat'] < 37.725) & (points['lat'] > 37.675) & (points['lon'] < 30.12) & (
-        #        points['lon'] > 30.09))]  # burdur
-        roi_points_20 = points_20[((points_20['lat20'] < 37.75) & (points_20['lat20'] > 37.65) & (
-                points_20['lon20'] < 31.625) & (points_20['lon20'] > 31.475))]  # beysehir
-        roi_points = points[((points['lat'] < 37.75) & (points['lat'] > 37.65) & (points['lon'] < 31.625) & (
-                points['lon'] > 31.475))]
+        roi_points_20 = points_20[(points_20['lat20'] < 37.740) & (points_20['lat20'] > 37.670)]  # burdur
+        roi_points = points[(points['lat'] < 37.740) & (points['lat'] > 37.670) ]  # burdur
+        #roi_points_20 = points_20[((points_20['lat20'] < 37.75) & (points_20['lat20'] > 37.65) & (
+        #       points_20['lon20'] < 31.625) & (points_20['lon20'] > 31.475))]  # beysehir
+        #roi_points = points[((points['lat'] < 37.75) & (points['lat'] > 37.65) & (points['lon'] < 31.625) & (
+        #       points['lon'] > 31.475))]
         return roi_points_20, roi_points
 
     def read_xlsx(self,path_20,path_1):
@@ -136,7 +138,8 @@ class s3_extraction(object):
                                              roi_points["tide1"][hz1_index]) + (roi_points["tide2"][hz1_index])
                             correction_list.append(correction)
                     correction = np.median(correction_list)
-                    water_surface_height = alt - (roi_points_20["range20"][hz20_index] + correction) - roi_points[
+                    water_surface_height = alt - (roi_points_20["range20"][hz20_index] +
+                                                  roi_points_20["iono20"][hz20_index] + correction) - roi_points[
                         "geoid"].median()
                 water_surface_height_list.append(water_surface_height)
             wl_median = np.median(water_surface_height_list)
@@ -213,27 +216,30 @@ class s3_extraction(object):
             uncertainities.append(self.statistics_list[i]['Uncertainity'])
             s3_date = ((os.path.split(os.path.split(self.xlsx_list_20hz[i])[0])[1]).split("__")[2]).split("_")[0]
             d = dateutil.parser.parse(s3_date)
-            s3_date = d.strftime('%m-%d-%Y %H:%M')
+            s3_date = d.strftime('%d-%m-%Y')
             dates.append(s3_date)
         result_dict = {"Date": dates,
                        "Water Surface Height Above Reference Datum":water_levels,
                        "Water Surface Height Uncertainty": uncertainities}
         results_df = pd.DataFrame.from_dict(result_dict)
-        writer = pd.ExcelWriter(self.output_folder + "\\Results.xlsx", engine='xlsxwriter')
+        writer = pd.ExcelWriter(self.output_folder + "/Results.xlsx", engine='xlsxwriter')
         results_df.to_excel(writer,index=False)
         writer.save()
+
+    def export_to_excel(self):
+        s3_extraction.find_nc_files(self)
+        s3_extraction.write_to_xlsx(self)
+
     def run_from_xlsx(self):
         print(f"Start time is: {self.start_time}")
         xlsx_list_20hz, xlsx_list_1hz = s3_extraction.find_xlsx_files(self)
         statistics_list= s3_extraction.read_and_calculate_from_xlsx(self)
         print(statistics_list)
         s3_extraction.report_output(self)
+        a=1
 
-    def export_to_excel(self):
-        s3_extraction.find_nc_files(self)
-        s3_extraction.write_to_xlsx(self)
 
 if __name__ == "__main__":
     s3_extraction_object = s3_extraction()
-    #s3_extraction_object.export_to_excel()
+    s3_extraction_object.export_to_excel()
     s3_extraction_object.run_from_xlsx()
